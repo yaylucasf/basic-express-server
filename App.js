0b83ca6504daff29a5c4020fe5483efd63a303b4
yaylucasf <yay.lucasfernandes@gmail.com>
2023-09-14 17:35:19 -0300

    '/users' endpoint added

--- App.js
+++ App.js
@@ -2,8 +2,32 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (_, response) => {
-    response.send('Hello, world!');
+const users = [
+    {
+        'name': 'Lucas Fernandes',
+        'age': 19,
+        'height': 1.83
+    }
+];
+
+app.get('/users', (_, response) => {
+    if (users.length === 0) {
+        response.send();
+        return;
+    }
+
+    response.send(users);
+});
+
+app.get('/users/:id', (request, response) => {
+    const id = request.params.id;
+
+    if (users[id] === undefined) {
+        response.send();
+        return;
+    }
+
+    response.send(users[id]);
 });
 
 app.listen(port, () => {
